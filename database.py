788d11295419d7f547897020282c9315ca849c4a
Davin Neilson <114067292+DNeilson67@users.noreply.github.com>
2024-09-03 17:05:22 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://default:z1puxRB8mwnk@ep-falling-pond-a1rnz83n.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.rdbcbzxvtuqhnxwvdtgr:YBLOgr0kcSsns3qt@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
