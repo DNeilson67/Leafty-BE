fb81b99a34ccdefe847be50ea0afb5079938f718
Davin Neilson <114067292+DNeilson67@users.noreply.github.com>
2024-09-03 16:21:12 +0700

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.rdbcbzxvtuqhnxwvdtgr:YBLOgr0kcSsns3qt@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
